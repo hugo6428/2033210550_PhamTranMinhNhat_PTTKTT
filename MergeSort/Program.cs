9004f496e710e8718e6acb6dc7e8b4bd2ae42a56
nhatp456@gmail.com <nhatp456@gmail.com>
2024-07-10 11:22:36 +0700

    Update Merge and Sum

---
+++ MergeSort/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class MergeSort
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.Write("\nNhap vao so luong phan tu cua mang: ");
+                n = int.Parse(Console.ReadLine());
+            } while (n <= 0);
+
+            int[] intArray = new int[n];
+            Console.WriteLine("Nhap vao cac phan tu cua mang: ");
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            SortMethod(intArray, 0, n - 1);
+            Console.Write("Cac phan tu sau khi sap xep: ");
+
+            foreach (int item in intArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+         }
+        static public void MergeMethod(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, left_end, num_elements, tmp_pos;
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+        static public void SortMethod(int[] numbers, int left, int right)
+        {
+            int mid;
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                SortMethod(numbers, left, mid);
+                SortMethod(numbers, (mid + 1), right);
+                MergeMethod(numbers, left, (mid + 1), right);
+            }
+        }
+    }
+}
---
+++ Nhan2so/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhan2so
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 6, 5, 9, 1, 0, 3, 7, 8 };
+            int max1 = arr[0], max2 = arr[0];
+            int place = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max1 < arr[i])
+                {
+                    max1 = arr[i];
+                    place = i;
+                }
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max2 < arr[i] && place != i)
+                {
+                    max2 = arr[i];
+                }
+
+            }
+            int sum = max1*max2;
+            Console.WriteLine("So trong mang: ");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("Tich cua 2 so lon nhat la: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
